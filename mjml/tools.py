162723d5418a08dffc426ed747126e4b95d66a4f
liminspace <igormeln@gmail.com>
2016-07-24 23:45:41 +0300

    Some fix

--- mjml/tools.py
+++ mjml/tools.py
@@ -2,7 +2,6 @@ import socket
 import random
 import subprocess
 from django.utils.encoding import force_str
-
 from . import settings as mjml_settings
 
 
@@ -26,9 +25,13 @@ def _mjml_render_by_cmd(mjml_code):
 
 
 def _mjml_render_by_tcpserver(mjml_code):
-    servers = list(mjml_settings.MJML_TCPSERVERS)[:]
-    random.shuffle(servers)
+    if len(mjml_settings.MJML_TCPSERVERS) > 1:
+        servers = list(mjml_settings.MJML_TCPSERVERS)[:]
+        random.shuffle(servers)
+    else:
+        servers = mjml_settings.MJML_TCPSERVERS
 
+    mjml_code = mjml_code.encode('utf8') or ' '
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     for host, port in servers:
         try:
@@ -36,7 +39,7 @@ def _mjml_render_by_tcpserver(mjml_code):
         except socket.error:
             continue
         try:
-            s.send(mjml_code.encode('utf8') or ' ')
+            s.send(mjml_code)
             ok = force_str(s.recv(1)) == '0'
             result_len = int(force_str(s.recv(9)))
             result = force_str(s.recv(result_len))
@@ -57,5 +60,4 @@ def mjml_render(mjml_code):
         return _mjml_render_by_cmd(mjml_code)
     elif mjml_settings.MJML_BACKEND_MODE == 'tcpserver':
         return _mjml_render_by_tcpserver(mjml_code)
-    else:
-        raise RuntimeError('Invalid settings.MJML_BACKEND_MODE "{}"'.format(mjml_settings.MJML_BACKEND_MODE))
+    raise RuntimeError('Invalid settings.MJML_BACKEND_MODE "{}"'.format(mjml_settings.MJML_BACKEND_MODE))
