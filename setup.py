03567ba68c12db5aec9636ecd24776098cb71317
Igor Melnyk <liminspace@gmail.com>
2018-08-07 18:05:46 +0300

    Added supporting MJML v4.1.2 Added supporting Django v2.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     zip_safe=False,  # because include static
     platforms=['OS Independent'],
     install_requires=[
-        'django>=1.8,<2.1',
+        'django>=1.8,<2.2',
     ],
     keywords=[
         'django', 'mjml', 'django-mjml', 'email', 'layout', 'template', 'templatetag',
