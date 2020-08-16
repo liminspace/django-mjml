28192d6c84abb64b19c0c43c92f9eb19a86b54de
Dani Hodovic <dani.hodovic@gmail.com>
2020-08-16 16:39:26 +0300

    Support Django 3.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     zip_safe=False,  # because include static
     platforms=['OS Independent'],
     install_requires=[
-        'django >=1.8,<3.1',
+        'django >=1.8,=<3.1',
     ],
     extras_require={
         'requests': [
